467ef3cc387738a541e8f8ea9ebd63a9a77f05a3
jherson.martelo <jherson.martelo@nexergroup.com>
2024-01-08 19:59:57 -0500

    feat(api): added create new room

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,7 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
+import Room from "../models/room";
 
 export const allRooms = (req: NextRequest) => {
     return NextResponse.json({
         data: "Hello"
+    })
+}
+
+export const newRoom = async (req: NextRequest) => {
+    const body = await req.json()
+
+    const room = await Room.create(body)
+
+    return NextResponse.json({
+        success: true,
+        room
     })
 }
