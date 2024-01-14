fd845b1bf83b1d24f7690e894ecbb58a4cc5f6d3
Jherson Martelo Medina <37153701+G0m1namm@users.noreply.github.com>
2024-01-13 22:20:06 -0500

    feat(header): create basic header component

---
+++ components/header.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import UserNav from "./user-nav"
+
+type Props = {}
+
+export default function Header({ }: Props) {
+    return (
+        <header className="w-full sticky top-0 left-0">
+            <div className="container h-16 flex items-center justify-between">
+                <h2 className="font-black text-lg">BookIt</h2>
+                <UserNav />
+            </div>
+        </header>
+    )
+}
