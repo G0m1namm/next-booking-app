4ff48efa9bfc9bb581e72400ddf3e84df76219f2
Jherson Martelo Medina <37153701+G0m1namm@users.noreply.github.com>
2024-01-13 22:19:47 -0500

    feat(footer): create basic footer component

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import UserNav from "./user-nav"
+
+type Props = {}
+
+export default function Footer({ }: Props) {
+    return (
+        <footer className="container flex justify-center items-center py-10">
+            Booking App - Jherson Martelo - All Rights Reserved
+        </footer>
+    )
+}
