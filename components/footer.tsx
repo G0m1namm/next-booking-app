760b43d049868868c366665e9cb978c0d6e68331
Jherson Martelo Medina <37153701+G0m1namm@users.noreply.github.com>
2024-01-17 19:37:32 -0500

    feat(footer): style: centered with a border to match header

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
     return (
-        <footer className="container flex justify-center items-center py-10">
-            Booking App - Jherson Martelo - All Rights Reserved
+        <footer className="w-full py-8 border-t border-slate-200">
+            <div className="page-shell-container mx-auto text-center">
+                Booking App - Jherson Martelo - All Rights Reserved
+            </div>
         </footer>
     )
 }
