43c8778786cef904a188555c7eea1a555dda34db
Jherson Martelo Medina <37153701+G0m1namm@users.noreply.github.com>
2024-06-15 11:23:40 -0500

    feat(ui): add PasswordInput component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 
+import { useToggle } from '@mantine/hooks';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+
 import { cn } from '@/lib/utils';
+
+import { Button } from './button';
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
@@ -21,4 +26,31 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 );
 Input.displayName = 'Input';
 
-export { Input };
+const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    const [open, toggle] = useToggle();
+    return (
+      <div className="relative">
+        <Input
+          ref={ref}
+          type={open ? 'text' : 'password'}
+          className={cn('pr-8', className)}
+          {...props}
+        />
+        <Button
+          type="button"
+          variant="ghost"
+          size="icon"
+          className="absolute top-1/2 transform -translate-y-1/2 right-1 h-7 w-7"
+          onClick={() => toggle()}
+        >
+          {open ? <EyeIcon className="h-4 w-4" /> : <EyeOffIcon className="h-4 w-4" />}
+          <span className="sr-only">Toggle password visibility</span>
+        </Button>
+      </div>
+    );
+  }
+);
+PasswordInput.displayName = 'PasswordInput';
+
+export { Input, PasswordInput };
